f9adcfe7503052759607af6dd996aa6bc5d81db1
Sergey Gorshkov <gorshkov88@gmail.com>
2020-12-31 21:00:31 +0400

    add test mocks

---
+++ test/mock.ts
@@ -0,0 +1,21 @@
+export const baseUrl: string = "https://test.ru";
+
+export const apiTree = {
+  first: "first",
+  second: "second",
+  nested: {
+    "nested-first": "nested-first",
+    "nested-second": "nested-second",
+    "nested-third": {
+      last: "/last",
+    },
+  },
+};
+
+export const expectedApi = {
+  first: `${baseUrl}/${apiTree.first}`,
+  second: `${baseUrl}/${apiTree.second}`,
+  nested: `${baseUrl}/nested`,
+  nestedNestedFirst: `${baseUrl}/nested/${apiTree.nested["nested-first"]}`,
+  nestedNestedThirdLast: `${baseUrl}/nested/${apiTree.nested["nested-third"].last}`,
+};
