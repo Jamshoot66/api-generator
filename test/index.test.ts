24cdc9b381074f1fab6f1a5f03fe5d50039889a0
Sergey Gorshkov <gorshkov88@gmail.com>
2020-12-31 21:00:52 +0400

    add single node test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,9 @@
 import apiGenerator from "../src/index";
-
+import { baseUrl, apiTree, expectedApi } from "./mock";
 
 describe("Testing api generator", () => {
-  it("you shouldn't pass!", () => {
-    const api = apiGenerator;
-    expect(api).toBe('no-way');
+  it("you shouldn't contain single nodes", () => {
+    const api = apiGenerator(baseUrl, apiTree);
+    expect(api.first).toBe(expectedApi.first);
   });
 });
