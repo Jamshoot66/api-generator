96535bacd8cf1bf0bbaceb20c9bce15d9ad7adce
Sergey Gorshkov <gorshkov88@gmail.com>
2020-12-31 21:02:28 +0400

    add nested node test

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,4 +6,9 @@ describe("Testing api generator", () => {
     const api = apiGenerator(baseUrl, apiTree);
     expect(api.first).toBe(expectedApi.first);
   });
+  
+  it("you shouldn't contain nested node name as url", () => {
+    const api = apiGenerator(baseUrl, apiTree);
+    expect(`${api.nested}`).toBe(expectedApi.nested);
+  });
 });
